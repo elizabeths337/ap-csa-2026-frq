8a7c575ae378c5a3f7ac806553a04c719d058431
elizabeths337 <elizabeths337@nycstudents.net>
2026-05-19 11:29:05 -0400

    Update part-1a.java

--- question-1/part-1a.java
+++ question-1/part-1a.java
@@ -1,1 +1,16 @@
+public class Account {
+  private String username; 
 
+  public Account(String requestedName) {
+    String user = requestedName;
+    if (isAvailable(user)) {
+      username = user;
+    } else {
+      int num = 1;
+      while (!(isAvailable(user))) {
+        user = user + Integer.toString(num);
+      } 
+      username = user;
+    }
+  }
+}
