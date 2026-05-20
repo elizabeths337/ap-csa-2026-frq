47e33b851c1fe37a3cff716fd8a652535e028191
elizabeths337 <elizabeths337@nycstudents.net>
2026-05-20 11:28:46 -0400

    Update part-1b.java

--- question-1/part-1b.java
+++ question-1/part-1b.java
@@ -1,1 +1,17 @@
-
+public String getShortenedName() {
+  String short = "";
+  if (username.substring(0,1).equals("-" || username.substring(substring.length() - 1).equals("-") || username.length() >= 2)) {
+    return username;
+  }
+  int consec = 0;
+  for (int i = 0; i < username.length(); i++ {
+    // add check if consec
+      
+  }
+  
+  for (int i = 0; i < username.length(); i++) {
+    if (username.indexOf("-") != -1) {
+      short += username.substring(i,i+1);
+    }
+  return short;
+}
